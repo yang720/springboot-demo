a2096da6e29d0c6a623ca2e38bcbbdccdaeff3fa
“yang720 <2216681165@qq.com>
2020-05-13 00:11:35 +0800

    修改配置注释

--- springboot-15-springcloud/consumer-user/src/main/java/com/ly/consumeruser/controller/UserController.java
+++ springboot-15-springcloud/consumer-user/src/main/java/com/ly/consumeruser/controller/UserController.java
@@ -16,6 +16,11 @@ public class UserController {
     @Resource
     RestTemplate restTemplate;
 
+    /**
+     * "http://PROVIDER-TICKET/ticket"为服务端xml配置的项目名下的ticket服务
+     * @param name 姓名
+     * @return String 购买结果
+     */
     @GetMapping("/buy")
     public String byTicket(String name){
         String ticket = restTemplate.getForObject("http://PROVIDER-TICKET/ticket", String.class);
